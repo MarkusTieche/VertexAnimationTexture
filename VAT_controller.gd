dace87f58fa027d9d5bee17037254326387140d5
MarkusTieche <tieche@gmx.com>
2026-04-04 10:11:28 +0200

    added proper vat instance shader

---
+++ VAT_controller.gd
@@ -0,0 +1,25 @@
+@tool
+extends Node
+
+@export var target_mesh: MeshInstance3D
+
+@export var startFrame:int = 0;
+@export var endFrame:int = 0;
+@export var frameRate:int = 0;
+@export var loop:bool = false;
+
+@export_tool_button("Play Animation","Play")
+var play_button = _on_play_pressed
+
+
+func _on_play_pressed():
+
+	# 🔥 Set instance uniforms
+	target_mesh.set_instance_shader_parameter("instanceStartTime", Time.get_ticks_msec() / 1000.0)
+	target_mesh.set_instance_shader_parameter("frame_start",startFrame)
+	target_mesh.set_instance_shader_parameter("frame_end", endFrame)
+	target_mesh.set_instance_shader_parameter("loop_animation", loop)
+
+
+func _on_button_button_down() -> void:
+	call_deferred("_on_play_pressed")
